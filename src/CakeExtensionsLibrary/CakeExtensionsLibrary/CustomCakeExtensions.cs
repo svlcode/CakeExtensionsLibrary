317cae4b3bf9aba8c5a491bbdec423236c6ce579
Vio Slaniceanu <viorels@netrom.ro>
2017-07-21 15:52:14 +0300

    Added alias method for reading passwords.

---
+++ src/CakeExtensionsLibrary/CakeExtensionsLibrary/CustomCakeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeExtensionsLibrary
+{
+    public static class CustomCakeExtensions
+    {
+        [CakeMethodAlias]
+        [CakeNamespaceImport("CakeExtensionsLibrary")]
+        public static string ReadConsolePassword(this ICakeContext context)
+        {
+            string pass = "";
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Backspace Should Not Work
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    pass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        pass = pass.Substring(0, (pass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            // Stops Receving Keys Once Enter is Pressed
+            while (key.Key != ConsoleKey.Enter);
+
+            return pass;
+        }
+    }
+}
